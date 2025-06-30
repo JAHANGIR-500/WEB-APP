6e7b6c9422df0b17648159ab25cf8f7828341106
JAHANGIR-500 <asikahmed5007@gmail.com>
2025-06-30 12:57:06 +0600

    Fix: Safe access to DATABASE_URL in production

--- erp/settings.py
+++ erp/settings.py
@@ -90,10 +90,12 @@ WSGI_APPLICATION = 'erp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import dj_database_url
 import os
+import dj_database_url
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ['DATABASE_URL'])
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3')
+    )
 }
 
 
